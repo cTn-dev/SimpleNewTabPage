5c34e036d87e4c57e4340c801b1300b5c4deb16a
cTn <ctndesigner@gmail.com>
2014-07-27 16:13:23 +0200

    add more option properties to options window

--- options.js
+++ options.js
@@ -12,12 +12,31 @@ $(document).ready(function() {
                 $('input.appsExtensionsVisible').prop('checked', CONFIGURATION.data.options.appsExtensionsVisible);
                 $('input.sessionsVisible').prop('checked', CONFIGURATION.data.options.sessionsVisible);
 
+                $('input.topSitesItemsMax').val(CONFIGURATION.data.options.topSitesItemsMax)
+                    .next()
+                    .text(CONFIGURATION.data.options.topSitesItemsMax);
+
+                $('input.sessionsItemsMax').val(CONFIGURATION.data.options.sessionsItemsMax)
+                    .next()
+                    .text(CONFIGURATION.data.options.sessionsItemsMax);
+
                 // bind events
-                $('div#options-window input').change(function() {
+                $('div#options-window input[type="checkbox"]').change(function() {
                     CONFIGURATION.status.optionsChanged = true;
 
                     var property = $(this).attr('class');
                     var val = $(this).is(':checked');
+
+                    CONFIGURATION.data.options[property] = val;
+                });
+
+                $('div#options-window input[type="range"]').on('input', function() {
+                    CONFIGURATION.status.optionsChanged = true;
+
+                    var property = $(this).attr('class');
+                    var val = $(this).val();
+
+                    $(this).next('.amount').text(val);
 
                     CONFIGURATION.data.options[property] = val;
                 });
